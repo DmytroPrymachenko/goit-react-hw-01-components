9a6bfccafedf1655e0af151e0c9fc5caeb73c686
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2023-12-05 19:17:26 +0200

    пуш

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import friends from '../json/friends.json';
+import user from '../json/user.json';
+import data from '../json/data.json';
+import transactions from '../json/transactions.json';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
+        flexDirection: 'column',
         alignItems: 'center',
-        fontSize: 40,
+        fontSize: 20,
         color: '#010101'
+        
       }}
     >
-      React homework template
+     
+      <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+     
+ 
+      <FriendList friends={friends} />;
+     <TransactionHistory items={transactions} />;  
     </div>
   );
 };
+// Тест
+// Тест
+
